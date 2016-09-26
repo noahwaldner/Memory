c22181353514535cf7280ee8b90d5b8e0aa567ea
noahwaldner <noah@noahwaldner.ch>
2016-09-26 08:08:23 +0200

    Initial commit

---
+++ app/src/main/java/com/example/user/memoryapp/MainActivity.java
@@ -0,0 +1,80 @@
+package com.example.user.memoryapp;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.zxing.client.android.Intents;
+import com.google.zxing.integration.android.IntentIntegrator;
+
+import java.io.File;
+
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Button scan1;
+    private TextView text1;
+    private ImageView image1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        scan1 = (Button) findViewById(R.id.button1);
+        text1 = (TextView) findViewById(R.id.textView1);
+        image1 = (ImageView) findViewById(R.id.imageView1);
+    }
+
+    protected void onResume(){
+        super.onResume();
+
+        scan1.setOnClickListener(this);
+
+
+    }
+
+    public void onClick(View v){
+        switch (v.getId()) {
+            case R.id.button1:
+                takeQrCodePicture();
+            break;
+
+        }
+
+    }
+
+    public void takeQrCodePicture() {
+        IntentIntegrator integrator = new IntentIntegrator(this);
+        integrator.setCaptureActivity(MyCaptureActivity.class);
+        integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES);
+        integrator.setOrientationLocked(false);
+        integrator.addExtra(Intents.Scan.BARCODE_IMAGE_ENABLED, true);
+        integrator.initiateScan();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == IntentIntegrator.REQUEST_CODE
+                && resultCode == RESULT_OK) {
+
+            Bundle extras = data.getExtras();
+            String path = extras.getString(
+                    Intents.Scan.RESULT_BARCODE_IMAGE_PATH);
+
+            // Ein Bitmap zur Darstellung erhalten wir so:
+            // Bitmap bmp = BitmapFactory.decodeFile(path)
+
+            String code = extras.getString(
+                    Intents.Scan.RESULT);
+
+            text1.setText(code);
+            image1.setImageURI(Uri.fromFile(new File(path)));
+        }
+    }
+}
---
+++ app/src/main/java/com/example/user/memoryapp/MyCaptureActivity.java
@@ -0,0 +1,8 @@
+package com.example.user.memoryapp;
+
+/**
+ * Created by user on 23.09.2016.
+ */
+import com.journeyapps.barcodescanner.CaptureActivity;
+
+public class MyCaptureActivity extends CaptureActivity { }
